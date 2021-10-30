80020abf2a6b98efb878e79296ed32744147597a
RJGATON007 <robertgaton2017@gmail.com>
2021-10-30 10:40:25 +0800

    4

--- START-insurance-main/js/app.js
+++ START-insurance-main/js/app.js
@@ -1,15 +1,37 @@
 // Variables
-
+const form = document.getElementById('request-quote');
 
 
 
 
 // Event Listeners 
-document.addEventListener('DOMContentLoaded', function() {
-    // create the <option> for the years.
-    const html = new HTMLUI();
-    html.displayYears();
-});
+eventListeners();
+
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', function() {
+        // create the <option> for the years.
+        const html = new HTMLUI();
+        html.displayYears();
+    });
+    
+    // when the form is submitted
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        // read the values from the form
+        const make = document.getElementById('make').value;
+        const year = document.getElementById('year').value;
+        // read the radio buttons
+        const level = document.querySelector('input [name"level"]:checked').value;
+
+        // check that all the fields have something
+        if( make === '' || year === '' || level === '' ) {
+            console.log('Error');
+        } else {
+            console.log('Alright!');
+        }
+    });    
+}
 
 
 
@@ -25,5 +47,13 @@ HTMLUI.prototype.displayYears = function() {
           min = max - 20;
 
     // generate the list with the latest 20 years
+    const selectYears = document.getElementById('year');
 
+    // print the values
+    for(let i = max; i >= min; i-- ) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYears.appendChild(option); 
+    }
 }
